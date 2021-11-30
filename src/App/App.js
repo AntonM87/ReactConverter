e29ca862b3dc973ff5541c264d6d66f4cb3582d9
Anton Martynenko <anton@MacBook-Air-Anton.local>
2021-11-30 23:37:21 +0500

    добавил 2 взаимоконвертируемых поля

--- src/App/App.js
+++ src/App/App.js
@@ -10,83 +10,57 @@ import {
 import Converter from "../Converter/Converter";
 import Rates from '../pages/RatesPage';
 import NotFoundPage from '../pages/NotFoundPage'
-import './styles.css';
 
 //https://currencylayer.com/
-
-const mainCurrency = 'USDRUB';
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             USDRUB: '',
-            input: '',
         }
-
-        this.handlerInputLengthValidation = this.handlerInputLengthValidation.bind(this);
     }
 
     async componentDidMount() {
         const cur = new CurRateAPI();
         this.setState({
             USDRUB: await cur.getUSDRUBcurrency(),
-            // input: localStorage.getItem(mainCurrency),
         })
     }
 
-    // componentWillUnmount() {
-    //     localStorage.setItem(mainCurrency,this.state.input);
+    // validation(number) {
+    //
+    //     if (number.length > 15) {
+    //         alert('Не более 15 цифр');
+    //         return number.substring(0, 15);
+    //     }
+    //
+    //     if (number === '') {
+    //         return '';
+    //     }
+    //
+    //     if (parseFloat(number) || parseInt(number)) {
+    //         if (number.match('[A-Za-z]+$') !== null) {
+    //             const index = number.match('[A-Za-z]+$')['index'];
+    //             alert('В числах есть буквы ?')
+    //             return number.substring(0, index);
+    //             // не работает с русским алфавитом
+    //         }
+    //         return number;
+    //     } else {
+    //         alert('Введите корректное значение');
+    //         return '';
+    //     }
     // }
 
-    validation(number) {
-
-        if (number.length > 15) {
-            alert('Не более 15 цифр');
-            return number.substring(0, 15);
-        }
-
-        if (number === '') {
-            return '';
-        }
-
-        if (parseFloat(number) || parseInt(number)) {
-            if (number.match('[A-Za-z]+$') !== null) {
-                const index = number.match('[A-Za-z]+$')['index'];
-                alert('В числах есть буквы ?')
-                return number.substring(0, index);
-                // не работает с русским алфавитом
-            }
-            return number;
-        } else {
-            alert('Введите корректное значение');
-            return '';
-        }
-    }
-
-    stateInput(inputValue) {
-        this.setState({input: inputValue});
-    }
-
-    handlerInputLengthValidation(e) {
-        e.target.value = this.validation(e.target.value);
-        this.setState({input: e.target.value});
-        e.preventDefault();
-    }
-
     render() {
-
-        // console.log(localStorage);
-
-        const {USDRUB: cur, input} = this.state;
+        const {USDRUB: cur,} = this.state;
         return (
             <div>
                 <Routes>
                     <Route path='/' element={
                         <Converter
-                            handlerInputLengthValidation={this.handlerInputLengthValidation}
                             cur={cur}
-                            input={input}
                         />
                     }/>
                     <Route path='/RatesPage' element={<Rates/>}>Далее</Route>
--- src/Converter/Converter.js
+++ src/Converter/Converter.js
@@ -1,18 +1,46 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-export default function Converter(props) {
-    const {cur, input, handlerInputLengthValidation} = props;
-    return (
-        //переписать класс и хранить стейт для запоминания инпута ??? Попробовать
-        // не сохдраняется инпут а результат сохраняется при возврате "назад"
-        <div className='container custom-container'>
-            <div className='logo'>Конвертировать доллары в рубли</div>
-            <p className='current-rate'>Текущий курс рубля к доллару {(+cur).toFixed(1)}</p>
-            <input onChange={handlerInputLengthValidation} type="text" placeholder='max 15 цифр'/>
-            <div className='result'>Результат: {+(cur * input).toFixed(1)} Rub</div>
-            {/*<div>Результат: {`${cur}  ${input}`}</div>*/}
-            <div className='rates-page-link'><Link to='/RatesPAge'>Далее</Link></div>
-        </div>
-    )
+export default class Converter extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.currency = this.props.cur;
+        this.state = {
+            result: '',
+        }
+        this.getResult = this.getResult.bind(this);
+    }
+
+    getResult(e) {
+        let value = e.target.value;
+        if (value.length === 0) return '';
+
+        const USD = document.querySelector('input[name=usd]');
+        const RUB = document.querySelector('input[name=rub]');
+
+        if (e.target.name === 'usd') {
+            RUB.value = (this.currency * value).toFixed(1);
+        }
+        if (e.target.name === 'rub') {
+            USD.value = (value / this.currency).toFixed(1);
+        }
+    }
+
+    render() {
+        return (
+            <div className='container custom-container'>
+                <div className='logo'>Конвертировать доллары в рубли</div>
+                <p className='current-rate'>Текущий курс рубля к доллару {(+this.currency).toFixed(1)}</p>
+                <div className='input-container'>
+                    <input onChange={this.getResult} name='usd' type="number"
+                           placeholder='USD'/>
+                    <i className="fas fa-exchange-alt"/>
+                    <input onChange={this.getResult} name="rub" type='number'
+                           placeholder='RUB'/>
+                </div>
+                <div className='rates-page-link'><Link to='/RatesPAge'>Далее</Link></div>
+            </div>
+        )
+    }
 }
